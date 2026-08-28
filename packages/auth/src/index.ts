754a7f7bbc119f68c4fb9630016b4c3d139c5ee7
John Allen Delos Reyes <53966880+swtmply@users.noreply.github.com>
2026-08-29 04:53:16 +0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,34 @@
+import { expo } from "@better-auth/expo";
+import { createDb } from "@ludika/db";
+import * as schema from "@ludika/db/schema/auth";
+import { env } from "@ludika/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN, "ludika://", "exp://", "http://localhost:8081"],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [expo()],
+  });
+}
+
+export const auth = createAuth();
